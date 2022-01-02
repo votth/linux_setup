1022423e0cdc60baba39b199e247fde3fbd425b6
votth <voth998@gmail.com>
2022-01-02 22:07:04 +0100

    theme.lua

--- update.sh
+++ update.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-cp -avr -t . ~/.xinitrc ~/.git* ~/.bashrc ~/.bash_aliases ~/.vimrc ~/.vim/after/ ~/.vim/colors/ ~/.config/alacritty/ ~/.config/awesome/autorun.sh ~/.config/awesome/rc.lua ~/.config/lf/ ~/.config/picom/ ~/.config/vimb/config ~/.config/vim/ ~/testPackages
+cp -avr -t . ~/.xinitrc ~/.git* ~/.bashrc ~/.bash_aliases ~/.vimrc ~/.vim/after/ ~/.vim/colors/ ~/.config/alacritty/ ~/.config/awesome/autorun.sh ~/.config/awesome/rc.lua ~/.config/awesome/themes/ ~/.config/lf/ ~/.config/picom/ ~/.config/vimb/config ~/.config/vim/ ~/testPackages
 
-# while IFS= read -r line; do
-#     cp -avr -t . $line
-#     # echo $line
-# done < txt/trackedFiles.txt
-
-# for path in txt/trackedFiles.txt; do
-#     cp -avr $path .
-# done
-
+echo '------------------------------------'
 mv config .config/vimb/
 mv -t .config/awesome/ autorun.sh rc.lua
-echo '------------------------------------'
+cp -avr -t .config/ themes/
 cp -avr -t .config/ alacritty/ lf/ picom/ vim/
 cp -avr -t .vim/ colors/ after/
-rm -rf alacritty lf picom colors after vim
+rm -rf alacritty lf picom colors after vim themes
 
 pacman -Qq > ./txt/installedPackages.txt
 echo '------------------------------------' && git st
