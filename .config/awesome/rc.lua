7d5ee1f58ef7b8ea5a32c6f0ca6798245b39336c
votth <voth998@gmail.com>
2022-03-12 23:38:56 +0100

    rc.lua resized show_help menu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -249,10 +249,18 @@ end)
 -- ))
 -- }}}
 
--- {{{ Key bindings
+-- {{{ Key bindings / Shortcut
 globalkeys = gears.table.join(
-    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
-              {description="show help", group="awesome"}),
+    -- awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+    --           {description="show help", group="awesome"}),
+
+    awful.key({ modkey }, "s",  function ()
+	    local screen = awful.screen.focused()
+	    hotkeys_popup.widget.new({
+		    width = 2400, height = 750, font = "Source Code Pro Bold 13",
+		    description_font = "Source Code Pro 12"}):show_help(nil, screen)
+	    end, {description = "show help", group="awesome"}),
+
     awful.key({ modkey,           }, ",",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, ".",  awful.tag.viewnext,
@@ -581,23 +589,15 @@ awful.rules.rules = {
       properties = { screen = 1, tag = "2" } },
     { rule = { class = "Spotify" },
       properties = { screen = 1, tag = "2" } },
-    { rule = { class = "Caprine" },
-      properties = { screen = 1, tag = "2" } },
-
-    -- { rule = { class = "firefox" },
-    --   properties = { screen = 1, tag = "3" } },
+    { rule = { class = "firefox" },
+      properties = { screen = 1, tag = "3" } },
     { rule = { class = "Tor Browser" },
       properties = { screen = 1, tag = "3" } },
-
     { rule = { class = "Vmplayer" },
       properties = { screen = 1, tag = "4" } },
     { rule = { class = "vlc" },
       properties = { screen = 1, tag = "4" } },
     { rule = { class = "libreoffice" },
-      properties = { screen = 1, tag = "4" } },
-    { rule = { class = "libreoffice-writer" },
-      properties = { screen = 1, tag = "4" } },
-    { rule = { class = "Microsoft Teams - Preview" },
       properties = { screen = 1, tag = "4" } },
 
 }
