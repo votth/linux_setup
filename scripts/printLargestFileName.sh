1528ee21d10678f41deaa4db73cb63f31a6cc956
votth <voth998@gmail.com>
2022-05-16 23:15:27 +0200

    scripts/printLargestFileName.sh

---
+++ scripts/printLargestFileName.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# create some foo files with different sizes
+for i in {1..5}
+do
+	dd if=/dev/zero of=file_$i.dat bs=1M count=1$i &> /dev/null
+done
+
+# return largest file's name
+du -h * | sort -nr | head -n 1 | cut -f 2-
