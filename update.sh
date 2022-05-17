1999306f248b64fb6889e0d9a3c68370b587a53e
votth <voth998@gmail.com>
2022-05-17 21:42:59 +0200

    Updated update.sh script with using 'xargs'

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cp -avr -t . ~/.xinitrc ~/.git* ~/.bashrc ~/.bash_aliases ~/.vimrc ~/.vim/after/ ~/.vim/colors/ ~/.config/alacritty/ ~/.config/awesome/autorun.sh ~/.config/awesome/rc.lua ~/.config/awesome/themes/ ~/.config/lf/ ~/.config/picom/ ~/.config/vimb/config ~/.config/vim/ ~/testPackages
-# cat ./txt/trackedFiles.txt | xargs cp -avr -t .
+# cp -avr -t . ~/.xinitrc ~/.git* ~/.bashrc ~/.bash_aliases ~/.vimrc ~/.vim/after/ ~/.vim/colors/ ~/.config/alacritty/ ~/.config/awesome/autorun.sh ~/.config/awesome/rc.lua ~/.config/awesome/themes/ ~/.config/lf/ ~/.config/picom/ ~/.config/vimb/config ~/.config/vim/ ~/testPackages
+cat ./txt/trackedFiles.txt | xargs -I '{}' cp -avr -t . '{}'
 
 echo '------------------------------------'
 mv config .config/vimb/
 mv -t .config/awesome/ autorun.sh rc.lua
 cp -avr -t .config/awesome/ themes/
-cp -avr -t .config/ alacritty/ lf/ picom/ vim/
+cp -avr -t .config/ alacritty/ picom/ vim/
 cp -avr -t .vim/ colors/ after/
-rm -rf alacritty lf picom colors after vim themes
+rm -rf alacritty picom colors after vim themes
 
 pacman -Qq > ./txt/installedPackages.txt
 echo '------------------------------------' && git st
